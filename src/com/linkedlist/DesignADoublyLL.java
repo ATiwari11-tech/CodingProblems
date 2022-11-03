febf368bcf887f79b9b4c64f77eecb20cbb908cd
AbhishekT <abhishek.tiwari@fox.com>
2022-11-03 20:17:21 +0530

    Added rearrange array with constant space

--- src/com/linkedlist/DesignADoublyLL.java
+++ src/com/linkedlist/DesignADoublyLL.java
@@ -11,22 +11,28 @@ class DNode{
     }
 }
 public class DesignADoublyLL {
-
-
     public static void main(String args[]){
         DNode dhead = new DNode(1);
         dhead.prev = null;
         createDoublyLL(dhead);
+        displayDoublyLL(dhead);
     }
-    public static Node createDoublyLL(DNode head){
+    public static DNode createDoublyLL(DNode head){
         DNode curr = head;
-        DNode prev = null;
         DNode newNode = null;
         for(int i=2;i<=8;i++){
            newNode = new DNode(i);
+           curr.next = newNode;
            newNode.prev = curr;
            curr = newNode;
         }
-        return null;
+        return head;
+    }
+    public static void displayDoublyLL(DNode head){
+        DNode curr = head;
+        while(curr != null){
+            System.out.print(curr.data+"->");
+            curr = curr.next;
+        }
     }
 }
