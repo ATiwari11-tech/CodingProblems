d19a7e1db171190cec86224e6c662e40f09327e8
AbhishekT <abhishek.tiwari@fox.com>
2023-02-19 08:55:14 +0530

    Added new BST problems

---
+++ src/com/trees/BSTNotes.java
@@ -0,0 +1,7 @@
+package com.trees;
+
+public class BSTNotes {
+    public static void main(String args[]){
+        //Inorder traversal of a binary search tree is always sorted in ascending order
+    }
+}
